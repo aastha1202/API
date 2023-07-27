c10c81e0914bbe7185bd128b7a2e0738571dbc52
Aastha Singh <saastha2002@gmail.com>
2023-07-27 08:34:09 -0700

    resolved issue

--- index.js
+++ index.js
@@ -32,11 +32,13 @@ con.on('open',function(){
 
 router.post("/todo", async (req, res) => {
     const tasks = req.body.task;
-    
+    const userTimezone = req.query.timezone;
     try {
       const savePromises = tasks.map(async (task) => {
         console.log(task.date)
-        const formattedDate = dayjs(task.date, 'ddd MMM DD YYYY').format('YYYY-MM-DD');
+        // const formattedDate = dayjs(task.date, 'ddd MMM DD YYYY').format('YYYY-MM-DD');
+        const formattedDate =DateTime.fromISO(task.date).setZone(userTimezone).toISODate();;
+
         // const options = {
         //   weekday: 'long',
         //   day: 'numeric',
@@ -58,20 +60,42 @@ router.post("/todo", async (req, res) => {
   
  
 
-  router.get("/todo", async (req, res) => {
+ router.get("/todo", async (req, res) => {
+  try {
+    const todayStart = DateTime.local().setZone('GMT').startOf('day')
+    const tomorrowStart = todayStart.plus({ days: 1 }).startOf('day');
+
+    console.log(todayStart.toJSDate());
+    console.log(tomorrowStart.toJSDate());
+
+    const todos = await Todo.find({
+      date: {
+        $gte: todayStart.toJSDate(),
+        $lt: tomorrowStart.toJSDate()
+      }
+    });
+
+    res.send(todos);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+
+  router.delete('/todo', async (req, res) => {
     try {
-      const today = DateTime.local().setZone('Asia/Kolkata').startOf('day');
-  
-      const todos = await Todo.find({
+      const today = DateTime.local().setZone('GMT').startOf('day');
+    
+      const result = await Todo.deleteMany({
         date: {
           $gte: today.toJSDate(),
           $lt: today.plus({ days: 1 }).toJSDate()
         }
       });
-  
-      res.send(todos);
+    
+      res.sendStatus(200); // Send a success status code
     } catch (err) {
-      res.send(err);
+      res.status(500).send(err);
     }
   });
   
@@ -114,6 +138,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 9000;
 }
+// let port = 9000
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
